2015c0c20956ce53279615653f85e6fb0d6efc18
Filipe Cajado <filipecajado@ucl.br>
2023-11-22 13:56:16 -0300

    add typeScript

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,5 @@
 import { Negociacao } from "./models/negociacao.js";
 
 const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-negociacao.qunatidade = 1000
-console.log(negociacao)
+console.log(negociacao.volume);
+
