1695683aa9ed9427527312b5361515a96fc02318
Filipe Cajado <filipecajado@ucl.br>
2024-01-17 23:24:33 -0300

    refactor class

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,26 +1,17 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor
-    }
-
-    get data(): Date{
-        return this._data;
-    }
-
-    get quantidade(): number{
-        return this._quantidade;
-    }
-    get valor(): number{
-        return this._valor;
+    constructor(
+        public _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number){
     }
 
     get volume(): number{
-        return this._valor * this._quantidade;
+        return this.valor * this.quantidade;
+    }
+
+    get data(): Date{
+        const data = new Date(this._data.getTime())
+        return data;
     }
 }
---
+++ app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from "./negociacao";
+
+export class Negociacoes{
+    private negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+
+    list(): readonly Negociacao[]{
+        return [...this.negociacoes];
+    }
+}
